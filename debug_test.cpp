20cab8df51de548c4899556ac335451ead90c26b
Joanne Santhosh <joanne.santhosh@mths.ca>
2022-10-05 21:17:47 +0000

    update

--- debug_test.cpp
+++ debug_test.cpp
@@ -12,7 +12,27 @@ int variableX = 25;
 void localVariable() {
     // This shows what happens with local variables
 
-    int variable_X = 10;
-    int variable_Y = 30;
-    int variable_Z = variable_X + variable_Y;
+    int variableX = 10;
+    int variableY = 30;
+    int variableZ = variableX + variableY;
+    std::cout << "Local variableX, variableY, variableZ: " << variableX
+              << " + " << variableY << " = " << variableZ << std::endl;
+}
+
+
+void globalVariable() {
+    // This shows what happens with global variables
+
+    variableX = variableX + 1;
+    int variableY = 30;
+    int variableZ = variableX + variableY;
+    std::cout << "Global variableX, variableY, variableZ: " << variableX
+              << " + " << variableY << " = " << variableZ << std::endl;
+}
+
+int main() {
+    // This function calls local and global
+
+    localVariable();
+    globalVariable();
 }
